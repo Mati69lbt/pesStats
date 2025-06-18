0765c31a0b658d131594c9b7673fb234524893ea
paranaSorteo <paranasorteo@gmail.com>
2025-06-18 14:48:52 -0300

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pesStats/", // 👈 nombre exacto del repositorio
   plugins: [react(), tailwindcss()],
 });
